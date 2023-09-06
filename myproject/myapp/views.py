dee93fe012a119508de5f00f6b68a8b35c327dc7
Apinun <apinun@Apinuns-MacBook-Pro-2.local>
2023-09-06 07:01:26 +0700

    update db sqlite

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from myapp.models import Person
 
 # Create your views here.
 def index(request):
-    name = "Apinun"
-    age = 24
-    return render(request, "index.html", {"age": age, "name": name})
+    allPerson = Person.objects.all()
+    return render(request, "index.html", {"allPerson": allPerson})
 
 def about(request):
     return render(request, "about.html")
